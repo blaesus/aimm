558c69e01dc4c06e7ad3b4685b7d91ca64ed2567
Andy Shu <shu@immux.com>
2023-05-05 18:10:06 +0800

    Make it work

--- ops/buildprod.sh
+++ ops/buildprod.sh
@@ -1,8 +1,14 @@
 echo "Deploying on remote machine..."
-cp ../package*.json . # Dockerfile can't copy from parent
+
+# Dockerfile can't copy from parent, we do it for it
+cd web;
+cp ../package*.json ./;
+mkdir data
+cp ../data/schema.prisma ./data/schema.prisma
+
 docker build --network="host" --no-cache -t aimm-docker . &&
 docker ps -aq | xargs docker stop | xargs docker rm;
 docker run -d --network="host" --add-host=host.docker.internal:172.17.0.1 aimm-docker;
-wait 5;
+sleep 5;
 curl localhost:3030/api/hello
 
