8468a9ee950c380a9edba3eb97fdae38dd4b5aeb
Andy Shu <shu@immux.com>
2023-05-07 22:33:54 +0800

    Kill all previous images

--- ops/update-api-image.sh
+++ ops/update-api-image.sh
@@ -1,5 +1,5 @@
 echo "Deploying on remote machine..."
 
 docker build --file api.dockerfile --network="host" --no-cache -t aimm-api . &&
-
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=aimm-api --format="{{.ID}}"))
 docker run -d -p 4000:4000 --add-host=host.docker.internal:127.0.0.1 aimm-api;
