995df40a83bf0b3e7d7e2552fd85a0ea8f6d4f08
Andy Shu <shu@immux.com>
2023-05-07 22:41:10 +0800

    Revise file

--- ops/update-api-image.sh
+++ ops/update-api-image.sh
@@ -1,5 +1,7 @@
 echo "Deploying on remote machine..."
 
 docker build --file api.dockerfile --network="host" --no-cache -t aimm-api . &&
+docker stop aimm-api-container;
+sleep 5;
 docker rm -f aimm-api-container &&
 docker run -d -p 4000:4000 --add-host=host.docker.internal:127.0.0.1 --name aimm-api-container aimm-api;
