9d51c02fc1bb303fc86008db2d33a8ed61b2c07b
Andy Shu <shu@immux.com>
2023-05-07 23:30:41 +0800

    Fix

--- ops/update-api-image.sh
+++ ops/update-api-image.sh
@@ -4,4 +4,4 @@ docker build --file api.dockerfile --network="host" --no-cache -t aimm-api . &&
 docker stop aimm-api-container;
 sleep 5;
 docker rm -f aimm-api-container &&
-docker run -d -p 4000:4000 --add-host=host.docker.internal:172.17.0.1 --name aimm-api-container aimm-api;
+docker run -d --netowork="host" --add-host=host.docker.internal:172.17.0.1 --name aimm-api-container aimm-api;
