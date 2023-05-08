23e09a60ca26982be6e37aadc0c951927e2efcd7
Andy Shu <shu@immux.com>
2023-05-08 17:28:07 +0800

    Add database code

--- ops/update-remote.sh
+++ ops/update-remote.sh
@@ -3,4 +3,6 @@ git checkout main &&
 git fetch origin main &&
 git reset --hard origin/main &&
 
+docker compose down
+npx prisma migrate deploy
 docker compose up -d --build --force-recreate --no-deps
