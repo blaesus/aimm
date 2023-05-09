5c192195cd718e419879d6e1a5327371ed519a14
Andy Shu <shu@immux.com>
2023-05-09 12:40:55 +0800

    Revise

--- data/spiders/mirror.ts
+++ data/spiders/mirror.ts
@@ -1,11 +1,20 @@
 import * as dotenv from "dotenv";
 import { obtainFiles } from "./obtain";
+import { prisma } from "../prismaClient";
 
 async function main() {
     dotenv.config();
-    await obtainFiles({
-        batchSize: 1,
-    })
+    // await obtainFiles({
+    //     batchSize: 1,
+    // })
+    // const repos = await prisma.repository.count({
+    //     where: {
+    //         favour: {
+    //             gt: 1000,
+    //         },
+    //     },
+    // });
+    // console.info(repos)
 }
 
 main();
