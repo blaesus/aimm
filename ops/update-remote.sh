e5cb888e06d1a2242ca27e38c7e11c278939398a
Andy Shu <shu@immux.com>
2023-05-09 12:47:24 +0800

    Revise process

--- ops/update-remote.sh
+++ ops/update-remote.sh
@@ -3,6 +3,7 @@ git checkout main &&
 git fetch origin main &&
 git reset --hard origin/main &&
 
+docker compose build
 docker compose down
 npx prisma migrate deploy
-docker compose up -d --build --force-recreate --no-deps
+docker compose up -d
