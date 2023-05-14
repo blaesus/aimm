d9506177906893e174dcdb756306751d619fc4d4
Andy Shu <shu@immux.com>
2023-05-14 10:53:27 +0800

    Fix

--- server/routes/hello.ts
+++ server/routes/hello.ts
@@ -4,5 +4,9 @@ import { prisma } from "../../data/prismaClient";
 export const hello = async (ctx: Koa.Context) => {
     const repos = await prisma.repository.count();
     const files = await prisma.fileRecord.count();
-    ctx.body = `Hello world from API with ${repos} repositories and ${files} files!`;
+    ctx.body = {
+        ok: true,
+        message: `Hello world from API with ${repos} repositories and ${files} files!`
+    }
+
 }
