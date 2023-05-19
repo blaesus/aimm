68ea980cf38388e8dea58b585d46a71c76a830f9
Andy Shu <shu@immux.com>
2023-05-19 16:19:10 +0800

    Revise

--- server/ai/remote-file-control.py
+++ server/ai/remote-file-control.py
@@ -5,62 +5,81 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
 class RequestHandler(BaseHTTPRequestHandler):
-
     def do_GET(self):
         if self.path == '/api/hello':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
+            self.send_header('Access-Control-Allow-Origin', '*')
             self.end_headers()
             self.wfile.write(b'OK')
         else:
             self.send_error(404)
 
     def do_POST(self):
-            content_length = int(self.headers['Content-Length'])
-            body = self.rfile.read(content_length)
-            urls = json.loads(body.decode('utf-8'))
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        urls = json.loads(body.decode('utf-8'))
 
-            if self.path == '/api/download':
-                self.send_response(200)
-                self.send_header('Content-type', 'text/plain')
-                self.end_headers()
+        if self.path == '/api/download':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
 
-                headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-                for url in urls:
-                    download_url = url['downloadUrl']
-                    file_name = os.path.basename(download_url)
-                    file_path = os.path.join('stable-diffusion-webui', 'models', 'Stable-diffusion', 'test', file_name)
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+            download_records = []
+            for url in urls:
+                download_url = url['downloadUrl']
+                file_name = os.path.basename(download_url)
+                file_path = os.path.join('models', file_name)
 
-                    try:
-                        req = urllib.request.Request(download_url, headers=headers)
-                        with urllib.request.urlopen(req) as response, open(file_path, 'wb') as out_file:
+                try:
+                    req = urllib.request.Request(download_url, headers=headers)
+                    with urllib.request.urlopen(req) as response:
+                        content_disposition = response.headers.get('Content-Disposition')
+                        if content_disposition:
+                            file_name = content_disposition.split('filename=')[1].strip('"')
+                            file_path = os.path.join('models', file_name)
+
+                        with open(file_path, 'wb') as out_file:
                             data = response.read()
                             out_file.write(data)
-                    except Exception as e:
-                        self.send_error(500, f"Failed to download {download_url}: {e}")
-                        return
 
-                self.wfile.write(b'Download completed successfully.')
+                    download_record = {
+                        'downloadUrl': download_url,
+                        'localUrl': f'http://{self.headers["Host"]}/{file_path}',
+                        'fileName': file_name,
+                        'fileSize': os.path.getsize(file_path)
+                    }
+                    download_records.append(download_record)
+                except Exception as e:
+                    download_record = {
+                        'downloadUrl': download_url,
+                        'error': str(e)
+                    }
+                    download_records.append(download_record)
 
-            elif self.path == '/api/delete':
-                self.send_response(200)
-                self.send_header('Content-type', 'text/plain')
-                self.end_headers()
+            self.wfile.write(json.dumps(download_records).encode('utf-8'))
 
-                for url in urls:
-                    file_name = os.path.basename(url['downloadUrl'])
-                    file_path = os.path.join('stable-diffusion-webui', 'models', 'Stable-diffusion', 'test', file_name)
+        elif self.path == '/api/clear':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
 
-                    try:
+            for file_name in os.listdir('models'):
+                file_path = os.path.join('models', file_name)
+                try:
+                    if os.path.isfile(file_path):
                         os.remove(file_path)
-                    except Exception as e:
-                        self.send_error(500, f"Failed to delete {file_path}: {e}")
-                        return
+                except Exception as e:
+                    self.send_error(500, f"Failed to delete {file_path}: {e}")
+                    return
 
-                self.wfile.write(b'Delete completed successfully.')
+            self.wfile.write(b'Clear completed successfully.')
 
-            else:
-                self.send_error(404)
+        else:
+            self.send_error(404)
 
 
 def run():
