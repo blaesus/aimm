482dee9a2564118452c0ed79a1acfde62e26ae0e
Andy Shu <shu@immux.com>
2023-05-19 16:43:09 +0800

    Revise

--- server/ai/remote-file-control.py
+++ server/ai/remote-file-control.py
@@ -1,106 +1,120 @@
 import os
 import json
-import urllib.request
-from urllib.parse import urlparse
+import requests
+from urllib.parse import unquote
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
-class RequestHandler(BaseHTTPRequestHandler):
+class MyRequestHandler(BaseHTTPRequestHandler):
+    download_dir = 'stable-diffusion-webui/models/Stable-diffusion/test/'
+
     def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        data = json.loads(body)
+
         if self.path == '/api/download':
-            self.handle_download_request()
-        elif self.path == '/api/ready':
-            self.handle_ready_request()
-        elif self.path == '/api/clear':
-            self.handle_clear_request()
-        else:
-            self.send_response(404)
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(b'Not found')
 
-    def handle_download_request(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        urls = json.loads(post_data)
+            download_records = []
+            for item in data:
+                download_url = item['downloadUrl']
+                local_filename = self.download_file(download_url)
+                record = {
+                    'downloadUrl': download_url,
+                    'localUrl': f'http://localhost:1234/{local_filename}',
+                    'filename': local_filename
+                }
+                download_records.append(record)
 
-        download_records = []
-        for url_data in urls:
-            download_url = url_data['downloadUrl']
-            local_filename = self.download_file(download_url)
-            download_record = {
-                'downloadUrl': download_url,
-                'localUrl': self.get_local_url(local_filename),
-                'filename': local_filename
-            }
-            download_records.append(download_record)
+            response = json.dumps({'downloads': download_records})
+            self.wfile.write(response.encode())
 
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(download_records).encode('utf-8'))
-
-    def download_file(self, url):
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-        req = urllib.request.Request(url, headers=headers)
-        response = urllib.request.urlopen(req)
-        content_disposition = response.headers.get('Content-Disposition')
-        if content_disposition:
-            filename = self.extract_filename(content_disposition)
-        else:
-            filename = self.guess_filename(url)
-        local_filename = os.path.join('stable-diffusion-webui/models/Stable-diffusion/test/', filename)
-        with open(local_filename, 'wb') as f:
-            f.write(response.read())
-        return local_filename
-
-    def extract_filename(self, content_disposition):
-        filename = content_disposition.split('filename=')[1].strip('\"\'')
-        return filename
-
-    def guess_filename(self, url):
-        parsed_url = urlparse(url)
-        filename = os.path.basename(parsed_url.path)
-        return filename
-
-    def get_local_url(self, local_filename):
-        base_url = 'http://localhost:1234/'
-        local_url = os.path.join(base_url, local_filename)
-        return local_url
-
-    def handle_ready_request(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        file_to_check = json.loads(post_data)['file']
-
-        local_filename = os.path.join('stable-diffusion-webui/models/Stable-diffusion/test/', file_to_check)
-        if os.path.exists(local_filename):
+        elif self.path == '/api/ready':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'File ready')
+
+            if 'filename' in data:
+                filename = unquote(data['filename'])
+                ready = self.is_file_ready(filename)
+                response = 'READY' if ready else 'NOT READY'
+                self.wfile.write(response.encode())
+
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'File not found')
+            self.wfile.write('404 Not Found'.encode())
 
-    def handle_clear_request(self):
-        file_dir = 'stable-diffusion-webui/models/Stable-diffusion/test/'
-        for filename in os.listdir(file_dir):
-            file_path = os.path.join(file_dir, filename)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
+    def do_GET(self):
+        if self.path == '/api/clear':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
 
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(b'Files cleared')
+            self.clear_directory()
+            self.wfile.write('Directory cleared'.encode())
+
+        elif self.path == '/api/hello':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write('OK'.encode())
+
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write('404 Not Found'.encode())
+
+    def download_file(self, url):
+        # Spoof the request with User-Agent
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(url, headers=headers, stream=True)
+
+        filename = self.get_filename(url, response.headers)
+        local_filepath = os.path.join(self.download_dir, filename)
+
+        with open(local_filepath, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+
+        return filename
+
+    def get_filename(self, url, headers):
+        # Try to extract the filename from the URL
+        filename = url.split('/')[-1]
+
+        # If the filename cannot be guessed, check the Content-Disposition header
+        if 'Content-Disposition' in headers:
+            disposition = headers['Content-Disposition']
+            if 'filename' in disposition:
+                filename = disposition.split('filename=')[1].strip('"')
+
+        return filename
+
+    def is_file_ready(self, filename):
+        filepath = os.path.join(self.download_dir, filename)
+        return os.path.exists(filepath)
+
+    def clear_directory(self):
+        for filename in os.listdir(self.download_dir):
+            filepath = os.path.join(self.download_dir, filename)
+            os.remove(filepath)
+
+    def log_message(self, format, *args):
+        # Disable logging to console
+        pass
 
 
 def run_server():
     server_address = ('localhost', 1234)
-    httpd = HTTPServer(server_address, RequestHandler)
-    print('Server running on localhost:1234...')
+    httpd = HTTPServer(server_address, MyRequestHandler)
+    print('Server running on localhost:1234')
     httpd.serve_forever()
 
 
