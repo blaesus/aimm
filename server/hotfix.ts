2211ea11d55b088ebd7d031b0bf9ec329c29213d
Andy Shu <shu@immux.com>
2023-05-19 10:13:53 +0800

    Add hotfix

--- server/hotfix.ts
+++ server/hotfix.ts
@@ -5,28 +5,42 @@ import * as dotenv from "dotenv";
 async function main() {
     dotenv.config();
     const prisma = new PrismaClient({});
-    const brokenFileRecords = await prisma.fileRecord.findMany({
-        where: {
-            hashA: "https://git-lfs.github.com/spec/v1",
-        },
-    });
-    const requester = makeRequester();
-    for (const r of brokenFileRecords) {
-        console.info(`Fixing ${r.filename}...`);
-        const hash = await requester.hashRemoteFile(r.downloadUrl);
-        console.info(`Calculated hash ${hash}...`);
-        if (hash) {
-            await prisma.fileRecord.update({
+    for (let page = 1; page < 1000; page += 1) {
+        const unsizedFileRecords = await prisma.fileRecord.findMany({
+            where: {
+                size: null,
+            },
+            take: 1000 * page,
+            skip: 1000 * (page - 1),
+        });
+        if (!unsizedFileRecords.length) {
+            break;
+        }
+        for (const file of unsizedFileRecords) {
+            console.info(`Checking size of file ${file.id} from ${file.downloadUrl}`);
+            const similarFiles = await prisma.fileRecord.findMany({
                 where: {
-                    id: r.id,
-                },
-                data: {
-                    hashA: hash,
-                    revisionId: r.revisionId,
+                    hashA: file.hashA,
                 },
             });
+            for (const alt of similarFiles) {
+                if (alt.size) {
+                    console.info(`Found ${alt.hashA} whose size is ${alt.size}`);
+                    await prisma.fileRecord.update({
+                        where: {
+                            id: file.id,
+                        },
+                        data: {
+                            size: alt.size,
+                        },
+                    });
+                    break;
+                }
+            }
         }
+
     }
+    console.info("hotfix finished")
 }
 
 main().catch(console.error);
