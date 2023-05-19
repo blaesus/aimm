8632a7c5dab52de06ce9ae56a8ff1b2abc6795ec
Andy Shu <shu@immux.com>
2023-05-19 15:54:55 +0800

    Revise

---
+++ server/ai/remote-file-control.py
@@ -0,0 +1,59 @@
+import json
+import os
+import urllib.request
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        urls = json.loads(body.decode('utf-8'))
+
+        if self.path == '/api/download':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+
+            for url in urls:
+                download_url = url['downloadUrl']
+                file_name = os.path.basename(download_url)
+                file_path = os.path.join('models', file_name)
+
+                try:
+                    urllib.request.urlretrieve(download_url, file_path)
+                except Exception as e:
+                    self.send_error(500, f"Failed to download {download_url}: {e}")
+                    return
+
+            self.wfile.write(b'Download completed successfully.')
+
+        elif self.path == '/api/delete':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+
+            for url in urls:
+                file_name = os.path.basename(url['downloadUrl'])
+                file_path = os.path.join('models', file_name)
+
+                try:
+                    os.remove(file_path)
+                except Exception as e:
+                    self.send_error(500, f"Failed to delete {file_path}: {e}")
+                    return
+
+            self.wfile.write(b'Delete completed successfully.')
+
+        else:
+            self.send_error(404)
+
+
+def run():
+    server_address = ('localhost', 3000)
+    httpd = HTTPServer(server_address, RequestHandler)
+    print(f"Server running on {server_address[0]}:{server_address[1]}")
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    run()
