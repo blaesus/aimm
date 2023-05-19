1de59c0934296148c9fb8a68db870ecb386bafa5
Andy Shu <shu@immux.com>
2023-05-19 10:45:15 +0800

    Revise

--- ops/deploy_nginx.sh
+++ ops/deploy_nginx.sh
@@ -1,5 +1,12 @@
-SERVER="62.210.95.35"
 SERVER_AUTH="ubuntu@${SERVER}"
+if [[ $1 == *"production"* ]]
+then
+  SERVER_AUTH=${AIMM_PROD_SERVER}
+fi
+if [[ $1 == *"stage"* ]]
+then
+  SERVER_AUTH=${AIMM_STAGE_SERVER}
+fi
 
 scp -P 22 ops/nginx/* ${SERVER_AUTH}:/tmp/;
 ssh -p 22 -t ${SERVER_AUTH} "sudo cp /tmp/*nginx* /etc/nginx/ && sudo nginx -t && sudo nginx -s reload";
