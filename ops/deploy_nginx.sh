c67ba54b07d108fc27a73ff0f21650cccee76c66
Andy Shu <shu@immux.com>
2023-05-19 11:02:44 +0800

    Add stage nginx config

--- ops/deploy_nginx.sh
+++ ops/deploy_nginx.sh
@@ -10,3 +10,7 @@ fi
 
 scp -P 22 ops/nginx/* ${SERVER_AUTH}:/tmp/;
 ssh -p 22 -t ${SERVER_AUTH} "sudo cp /tmp/*nginx* /etc/nginx/ && sudo nginx -t && sudo nginx -s reload";
+if [[ $1 == *"stage"* ]]
+then
+  ssh -p 22 -t ${SERVER_AUTH} "sudo cp /etc/nginx/routes.stage.nginx /etc/nginx/routes.nginx";
+fi
