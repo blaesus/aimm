cb9201a5adfc90dd653af20a36722e434a5f94a9
Andy Shu <shu@immux.com>
2023-05-21 08:34:46 +0800

    Revise

--- server/ai/remote/remote.js
+++ server/ai/remote/remote.js
@@ -1,99 +1,14 @@
-const http = require('http');
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const router = require('koa-router')();
 const fs = require('fs');
 const path = require('path');
-const { URL } = require('url');
 const { exec } = require('child_process');
 
-
 const SUB_DIR = "for_bench"
+const downloadDir = path.join(__dirname, `stable-diffusion-webui/models/Stable-diffusion/${SUB_DIR}`);
 
-const downloadDir = path.join(`/workspace/stable-diffusion-webui/models/Stable-diffusion/${SUB_DIR}`);
-
-const server = http.createServer((req, res) => {
-    const { method, url } = req;
-    const { pathname } = new URL(url, `http://${req.headers.host}`);
-
-    console.info("Request: ", method, pathname);
-
-    if (method === 'POST' && pathname === '/api/download') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk;
-        });
-        req.on('end', async () => {
-            try {
-                console.info("parsing")
-                const downloadList = JSON.parse(body);
-                console.info("parsed", downloadList)
-
-                for (const item of downloadList) {
-                    const { downloadUrl, filename } = item;
-                    const path = `${downloadDir}/${filename}`;
-                    downloadFile(downloadUrl, path)
-                }
-                res.setHeader('Content-Type', 'application/json');
-                res.end(JSON.stringify({ok: true}));
-            } catch (error) {
-                res.statusCode = 400;
-                res.end(JSON.stringify({
-                    ok: false,
-                    reason: error,
-                    data: body
-                }));
-            }
-        });
-    } else if (method === 'POST' && pathname === '/api/ready') {
-        let body = '';
-        req.on('data', (chunk) => {
-            body += chunk.toString();
-        });
-        req.on('end', () => {
-            try {
-                const data = JSON.parse(body);
-                const readyStatus = data.map((filename) => {
-                    const filePath = path.join(downloadDir, filename);
-                    return {
-                        filename,
-                        ready: fs.existsSync(filePath),
-                    };
-                });
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                res.end(JSON.stringify(readyStatus));
-            } catch (error) {
-                res.writeHead(400, {'Content-Type': 'application/json'});
-                res.end(JSON.stringify({error: 'Invalid JSON data'}));
-            }
-        });
-    } else if (method === 'POST' && pathname === '/api/clear') {
-        fs.readdir(downloadDir, (error, files) => {
-            if (error) {
-                res.statusCode = 500;
-                res.end('Internal Server Error');
-            } else {
-                files.forEach(file => {
-                    const filePath = path.join(downloadDir, file);
-                    fs.unlinkSync(filePath);
-                });
-                res.end('Directory cleared');
-            }
-        });
-    } else if (method === 'GET' && pathname === '/api/hello') {
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ ok: true }));
-    } else {
-        res.statusCode = 404;
-        res.end({
-            ok: false,
-            reason: 'path not found',
-            data: {
-                method,
-                pathname,
-            }
-        });
-    }
-});
-
-function downloadFile(url, filepath) {
+async function downloadFile(url, filepath) {
     console.info("Downloading url: ", url, " to ", filepath);
     const tempPath = `${filepath}.tmp`;
     const finalPath = filepath;
@@ -110,23 +25,50 @@ function downloadFile(url, filepath) {
                     console.info(`Downloaded ${url} to ${finalPath}`)
                 }
             })
-
         }
     })
 }
 
-function getFileName(url) {
-    const contentDisposition = headers['content-disposition'];
-    if (contentDisposition) {
-        const matches = contentDisposition.match(/filename="([^"]+)"/);
-        if (matches) {
-            return matches[1];
-        }
+router.post('/api/download', async (ctx) => {
+    const downloadList = ctx.request.body;
+    console.info("parsed", downloadList)
+    for (const item of downloadList) {
+        const { downloadUrl, filename } = item;
+        const path = `${downloadDir}/${filename}`;
+        await downloadFile(downloadUrl, path)
     }
+    ctx.body = {ok: true};
+})
 
-    return path.basename(new URL(url).pathname);
-}
+router.post('/api/ready', async (ctx) => {
+    const data = ctx.request.body;
+    const readyStatus = data.map((filename) => {
+        const filePath = path.join(downloadDir, filename);
+        return {
+            filename,
+            ready: fs.existsSync(filePath),
+        };
+    });
+    ctx.body = readyStatus;
+})
 
-server.listen(1234, () => {
+router.post('/api/clear', async (ctx) => {
+    const files = fs.readdirSync(downloadDir);
+    files.forEach(file => {
+        const filePath = path.join(downloadDir, file);
+        fs.unlinkSync(filePath);
+    });
+    ctx.body = 'Directory cleared';
+})
+
+router.get('/api/hello', async (ctx) => {
+    ctx.body = { ok: true };
+})
+
+const app = new Koa();
+app.use(bodyParser());
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(1234, () => {
     console.log('Server is running on http://localhost:1234');
 });
