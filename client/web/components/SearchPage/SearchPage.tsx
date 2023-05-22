6b8911442628e9315c27d83d96e4042d213355c3
Andy Shu <shu@immux.com>
2023-05-22 12:06:28 +0800

    Revise

--- client/web/components/SearchPage/SearchPage.tsx
+++ client/web/components/SearchPage/SearchPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { FileRecord, Registry, Repository, Revision } from "@prisma/client";
 import "./SearchPage.css";
-import { RepoCard } from "../RepoCard/RepoCard";
+import { FileList, RepoCard } from "../RepoCard/RepoCard";
 import { ClientState } from "../../reducer/state";
 import { ClientAction } from "../../reducer/action";
 import { Button } from "../Button/Button";
+import { match } from "assert";
 
 export function SearchPage(props: {
     state: ClientState,
@@ -18,24 +20,39 @@ export function SearchPage(props: {
     return (
         <div className="SearchPage">
             <div>
-                {
-                    matchedItems.reposByName.map(id => {
-                        const repo = repositories[id];
-                        if (!repo) {
-                            console.warn(`Missing repo ${id}`);
-                            return null;
-                        }
-                        return (
-                            <RepoCard
-                                key={repo.id}
-                                repoId={repo.id}
-                                repositories={repositories}
-                                revisions={revisions}
-                                fileRecords={fileRecords}
-                            />
-                        );
-                    })
-                }
+                <section>
+                    <h3>Matched repos</h3>
+                    {
+                        matchedItems.reposByName.map(id => {
+                            const repo = repositories[id];
+                            if (!repo) {
+                                console.warn(`Missing repo ${id}`);
+                                return null;
+                            }
+                            return (
+                                <RepoCard
+                                    key={repo.id}
+                                    repoId={repo.id}
+                                    repositories={repositories}
+                                    revisions={revisions}
+                                    fileRecords={fileRecords}
+                                />
+                            );
+                        })
+                    }
+                </section>
+                <section>
+                    <h3>Matched files</h3>
+                    <FileList
+                        files={matchedItems.filesByHash.map(id => fileRecords[id]).filter(Boolean)}
+                        showRepo={true}
+                        repositories={repositories}
+                        revisions={revisions}
+                        showBench={true}
+                        benchmarks={benchmarks}
+                        benchmarkResults={benchmarkResults}
+                    />
+                </section>
                 {
                     Object.keys(repositories).length >= itemLimit &&
                     <Button onClick={() => setItemLimit(Infinity)}>View all</Button>
