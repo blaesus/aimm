b4f758f9d3f406d42280c6908386e1665e201f33
Andy Shu <shu@immux.com>
2023-05-27 21:05:05 +0800

    Fix reverse proxy

--- dev.sh
+++ dev.sh
@@ -1,6 +1,6 @@
 nodemon --watch "./**" --ext "ts" server/api.ts &
 nodemon --watch "./**" --ext "ts" server/admin-api.ts &
-npx http-server -p 8010 client/web &
+npx http-server -p 8010 -a localhost client/web &
 sleep 3
 ./node_modules/.bin/webpack-dev-server --config client/web/webpack.config.js
 
