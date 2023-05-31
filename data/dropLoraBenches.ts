aeb31804e5146a3f67a558a6a04b9e8ebb4a8130
Andy Shu <shu@immux.com>
2023-05-31 10:49:48 +0800

    Fix

--- data/dropLoraBenches.ts
+++ data/dropLoraBenches.ts
@@ -15,21 +15,42 @@ async function fix() {
             },
         },
     });
+    console.info(`Found ${benchResults.length} results to delete`)
     const fileIds = benchResults.map(b => b.resultFileId);
-    await prisma.fileRecord.deleteMany({
-        where: {
-            id: {
-                in: fileIds,
-            },
-        },
-    });
     await prisma.benchmarkResult.deleteMany({
         where: {
             id: {
                 in: benchResults.map(b => b.id),
+            },
+        },
+    });
+    const links = await prisma.fileStorageRecordOnFileRecord.findMany({
+        where: {
+            fileRecordId: {
+                in: fileIds,
+            },
+        },
+    });
+    for (const link of links) {
+        await prisma.fileStorageRecordOnFileRecord.delete({
+            where: {
+                fileStorageRecordId_fileRecordId: {
+                    fileStorageRecordId: link.fileStorageRecordId,
+                    fileRecordId: link.fileRecordId,
+                }
             }
-        }
-    })
+        });
+        await prisma.fileRecord.delete({
+            where: {
+                id: link.fileRecordId,
+            },
+        });
+        await prisma.fileStorageRecord.delete({
+            where: {
+                id: link.fileStorageRecordId,
+            },
+        });
+    }
 }
 
 fix();
